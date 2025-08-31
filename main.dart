cf1c9f441ce54bfa7f403260ffd9c603a842cb1e
angelle joshe <angellejosher@gmail.com>
2025-08-31 09:22:29 -0400

    comments for main.dart

--- main.dart
+++ main.dart
@@ -1,18 +1,23 @@
 import 'package:flutter/material.dart';
 import 'screens/home_screen.dart';
 
+// Main function - entry point of the Flutter application
 void main() {
   runApp(const MysticApp());
 }
 
+// Root widget of the MysticApp application
 class MysticApp extends StatelessWidget {
   const MysticApp({super.key});
 
   @override
   Widget build(BuildContext context) {
+    // Configure and return the main MaterialApp widget
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      // Set the HomeScreen as the initial screen of the app
       home: const HomeScreen(),
     );
   }
 }
+
