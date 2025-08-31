91d74bd3493f696a632256601c2eeffd9347cff2
angelle joshe <angellejosher@gmail.com>
2025-08-31 09:27:29 -0400

    comments for hover_box.dart

--- hover_box.dart
+++ hover_box.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+// Custom widget to create an interactive box with hover animations
 class HoverBox extends StatefulWidget {
   final String imagePath;
   final String? imageLock;
@@ -17,17 +18,21 @@ class HoverBox extends StatefulWidget {
 }
 
 class _HoverBoxState extends State<HoverBox> {
+  // Track hover state for animations
   bool _isHovered = false;
   @override
   Widget build(BuildContext context) {
     return MouseRegion(
+      // Handle mouse enter and exit events for hover effect
       onEnter: (_) => setState(() => _isHovered = true),
       onExit: (_) => setState(() => _isHovered = false),
       child: AnimatedContainer(
+        // Animate size changes based on hover state
         width: _isHovered ? 160 : 150,
         height: _isHovered ? 160 : 150,
         duration: const Duration(milliseconds: 200),
         curve: Curves.easeInOut,
+        // Box styling with custom colors and shadow
         decoration: BoxDecoration(
           color: const Color(0xFF437592),
           borderRadius: BorderRadius.circular(30),
@@ -38,8 +43,10 @@ class _HoverBoxState extends State<HoverBox> {
         child: Stack(
           alignment: Alignment.center,
           children: [
+            // Main background image
             Image.asset(widget.imagePath, fit: BoxFit.cover),
 
+            // Optional overlay layer
             if (widget.overlay)
               Container(
                 width: double.infinity,
@@ -49,7 +56,8 @@ class _HoverBoxState extends State<HoverBox> {
                   borderRadius: BorderRadius.circular(30),
                 ),
               ),
-
+            
+            // Optional lock image that scales with hover
             if (widget.imageLock != null)
               Align(
                 alignment: Alignment.center,
@@ -65,3 +73,4 @@ class _HoverBoxState extends State<HoverBox> {
     );
   }
 }
+
