5abe36e610158bfbc7b9ea92d66e853032be0045
Gian Kyle Ibe <giankyleibe09@gmail.com>
2025-08-31 18:06:06 +0800

    Update home_screen.dart

--- home_screen.dart
+++ home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mystic_pick/screens/hover_box.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -18,21 +19,95 @@ class HomeScreen extends StatelessWidget {
         ),
         child: Center(
           child: Column(
+            mainAxisSize: MainAxisSize.min,
             children: [
-              const SizedBox(height: 50),
-              Image.asset('assets/name.png', width: 300),
+              Text(
+                'F8HUB',
+                style: GoogleFonts.pressStart2p(
+                  fontSize: 60,
+                  color: const Color(0xFFead8b1),
+                ),
+              ),
               const SizedBox(height: 50),
               Column(
                 mainAxisSize: MainAxisSize.min,
-                children: const [
+                children: [
                   Row(
                     mainAxisAlignment: MainAxisAlignment.center,
-                    children: [HoverBox(), SizedBox(width: 20), HoverBox()],
+                    children: [
+                      Column(
+                        children: [
+                          HoverBox(imagePath: 'assets/8ball.png'),
+                          SizedBox(height: 10),
+                          Text(
+                            'F8BALL',
+                            style: GoogleFonts.pressStart2p(
+                              color: Color(0xFFead8b1),
+                              fontSize: 20,
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(width: 20),
+                      Column(
+                        children: [
+                          HoverBox(
+                            imagePath: 'assets/cookie.png',
+                            imageLock: 'assets/lock.png',
+                            overlay: true,
+                          ),
+                          SizedBox(height: 10),
+                          Text(
+                            'F8COOKIE',
+                            style: GoogleFonts.pressStart2p(
+                              color: Color(0xFFead8b1),
+                              fontSize: 20,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
                   ),
                   SizedBox(height: 20),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.center,
-                    children: [HoverBox(), SizedBox(width: 20), HoverBox()],
+                    children: [
+                      Column(
+                        children: [
+                          HoverBox(
+                            imagePath: 'assets/dice.png',
+                            imageLock: 'assets/lock.png',
+                            overlay: true,
+                          ),
+                          SizedBox(height: 10),
+                          Text(
+                            'F8DICE',
+                            style: GoogleFonts.pressStart2p(
+                              color: Color(0xFFead8b1),
+                              fontSize: 20,
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(width: 20),
+                      Column(
+                        children: [
+                          HoverBox(
+                            imagePath: 'assets/TorD.png',
+                            imageLock: 'assets/lock.png',
+                            overlay: true,
+                          ),
+                          SizedBox(height: 10),
+                          Text(
+                            'F8TD',
+                            style: GoogleFonts.pressStart2p(
+                              color: Color(0xFFead8b1),
+                              fontSize: 20,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
                   ),
                 ],
               ),
