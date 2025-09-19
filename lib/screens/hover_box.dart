e1164442ba9d764d5498ec39fdffae3db279330e
Gian Kyle Ibe <giankyleibe09@gmail.com>
2025-09-20 01:01:09 +0800

    Update hover_box.dart

--- lib/screens/hover_box.dart
+++ lib/screens/hover_box.dart
@@ -2,15 +2,8 @@ import 'package:flutter/material.dart';
 
 class HoverBox extends StatefulWidget {
   final String imagePath;
-  final String? imageLock;
-  final bool overlay;
 
-  const HoverBox({
-    super.key,
-    required this.imagePath,
-    this.imageLock,
-    this.overlay = false,
-  });
+  const HoverBox({super.key, required this.imagePath});
 
   @override
   State<HoverBox> createState() => _HoverBoxState();
@@ -29,7 +22,12 @@ class _HoverBoxState extends State<HoverBox> {
         duration: const Duration(milliseconds: 200),
         curve: Curves.easeInOut,
         decoration: BoxDecoration(
-          color: const Color(0xFF437592),
+          gradient: LinearGradient(
+            begin: Alignment.centerLeft,
+            end: Alignment.centerRight,
+            colors: <Color>[Color(0xFF1B3B51), Color(0xFF3a6d8c)],
+          ),
+          border: Border.all(color: Color(0xFF3a6d8c), width: 2),
           borderRadius: BorderRadius.circular(30),
           boxShadow: const [
             BoxShadow(color: Color(0xFFEAD8B1), offset: Offset(4, 6)),
@@ -37,29 +35,7 @@ class _HoverBoxState extends State<HoverBox> {
         ),
         child: Stack(
           alignment: Alignment.center,
-          children: [
-            Image.asset(widget.imagePath, fit: BoxFit.cover),
-
-            if (widget.overlay)
-              Container(
-                width: double.infinity,
-                height: double.infinity,
-                decoration: BoxDecoration(
-                  color: Color(0x87437592),
-                  borderRadius: BorderRadius.circular(30),
-                ),
-              ),
-
-            if (widget.imageLock != null)
-              Align(
-                alignment: Alignment.center,
-                child: Image.asset(
-                  widget.imageLock!,
-                  width: _isHovered ? 150 : 130,
-                  height: _isHovered ? 150 : 130,
-                ),
-              ),
-          ],
+          children: [Image.asset(widget.imagePath, fit: BoxFit.cover)],
         ),
       ),
     );
